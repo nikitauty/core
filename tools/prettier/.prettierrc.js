67b1619db2a603a9be1df1d2819cfa6e97da7299
Nikita <neavinstasi@icloud.com>
2024-07-13 00:28:36 +0300

    init :apple:

---
+++ tools/prettier/.prettierrc.js
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+	printWidth: 100,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	trailingComma: 'es5',
+	semi: true,
+	tabWidth: 2,
+	useTabs: false,
+	endOfLine: 'lf',
+	arrowParens: 'always',
+}
